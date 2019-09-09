9e0efce41867dbb45272093694b7054f959ca5d9
Radoslaw Guzinski <radosuav@op.pl>
2019-09-09 11:42:56 +0200

    Fix snappy IO functions.

--- snappy_utils.py
+++ snappy_utils.py
@@ -3,8 +3,7 @@ import numpy as np
 import os
 import sys
 sys.path.append(os.path.join(os.path.expanduser("~"), ".snap", "snap-python"))
-from snappy import ProductIO, Product, ProductData #, jpy
-#Float = jpy.get_type('java.lang.Float')
+from snappy import ProductIO, Product, ProductData, String
 
 
 def read_snappy_product(file_path, band_name):
@@ -14,7 +13,11 @@ def read_snappy_product(file_path, band_name):
     geo_coding = prod.getSceneGeoCoding()
     data = np.empty((width, height))
     band = prod.getBand(band_name)
-    band.readPixels(0, 0, width, height, data)
+    try:
+        band.readPixels(0, 0, width, height, data)
+    except AttributeError:
+        prod.closeIO()
+        raise RuntimeError(file_path + " does not contain band " + band_name)
     prod.closeIO()
     return data, geo_coding
 
@@ -23,7 +26,17 @@ def write_snappy_product(file_path, bands, product_name, geo_coding):
     (width, height) = bands[0]['band_data'].shape
     product = Product(product_name, product_name, width, height)
     product.setSceneGeoCoding(geo_coding)
+
+    # Ensure that output is saved in BEAM-DIMAP format, otherwise writeHeader does not work.
+    file_path = os.path.splitext(file_path)[0] + '.dim'
+
+    # Bands have to be created before header is written but header has to be written before band
+    # data is written.
     for b in bands:
         band = product.addBand(b['band_name'], ProductData.TYPE_FLOAT32)
-        band.setPixels(0, 0, width, height, b['band_data'].astype(np.float32).tolist())
-    ProductIO.writeProduct(product, file_path, 'GeoTIFF')
+    product.setProductWriter(ProductIO.getProductWriter('BEAM-DIMAP'))
+    product.writeHeader(String(file_path))
+    for b in bands:
+        band = product.getBand(b['band_name'])
+        band.writePixels(0, 0, width, height, b['band_data'])
+    product.closeIO()
